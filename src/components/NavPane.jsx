5fbeb21740865e09a1e7579fada35eb075fcf321
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-04-30 16:22:56 +0300

    Add app components

---
+++ src/components/NavPane.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
+const NavPane = ({ title, reference }) => {
+  return (
+    <div className="d-flex align-items-center">
+      <Button className="me-2 link-dark" as={Link} to={reference} variant="link">
+        <i className="bi bi-chevron-left" />
+      </Button>
+      <p className="h5 m-0">{title}</p>
+    </div>
+  );
+};
+
+export default NavPane;
---
+++ src/components/TaskForm.jsx
@@ -0,0 +1,72 @@
+import { useState, useEffect, useRef } from 'react';
+import { useFormik } from 'formik';
+import { Button, Form } from 'react-bootstrap';
+
+const TaskForm = ({ values, actionType, handleSubmit }) => {
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  const inputRef = useRef(null);
+
+  useEffect(() => inputRef.current.focus(), []);
+
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      date: '',
+      description: '',
+      completed: false,
+      ...values,
+    },
+    onSubmit: (values) => handleSubmit(values, () => setIsInvalid(true)),
+  });
+
+  return (
+    <div className="task-form mx-auto">
+      <Form onSubmit={formik.handleSubmit}>
+        <Form.Group className="mb-4" controlId="title">
+          <Form.Label>Title *</Form.Label>
+          <Form.Control
+            ref={inputRef}
+            type="text"
+            name="title"
+            defaultValue={values?.title}
+            isInvalid={isInvalid}
+            placeholder="Daily meeting"
+            onChange={formik.handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-4" controlId="date">
+          <Form.Label>Date *</Form.Label>
+          <Form.Control
+            type="date"
+            name="date"
+            defaultValue={values?.date}
+            isInvalid={isInvalid}
+            onChange={formik.handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-4" controlId="description">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            name="description"
+            defaultValue={values?.description}
+            isInvalid={isInvalid}
+            onChange={formik.handleChange}
+            rows={4}
+          />
+          <Form.Control.Feedback type="invalid">
+            Something went wrong. Try again.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Button className="w-100" variant="warning" type="submit">
+          {actionType}
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default TaskForm;
