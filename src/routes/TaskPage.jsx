8cec2aa680d4e2056f9f19a1735716d7586f488e
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-05-05 17:44:50 +0300

    Add task page

--- src/routes/TaskPage.jsx
+++ src/routes/TaskPage.jsx
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import { useNavigate, useParams, Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import { Main, NavPane, Checkbox, ErrorMessage } from '../components/';
 import useDatabase from '../hooks/useDatabase';
 import { routes } from './';
 
 const TaskPage = () => {
+  const [isErrorShown, setIsErrorShown] = useState(false);
   const { listNum, taskId } = useParams();
-  const { readTask, deleteTask } = useDatabase();
+  const { readTask, deleteTask, updateTask } = useDatabase();
   const navigate = useNavigate();
 
   const [taskKey, task = {}] = readTask('id', taskId);
@@ -15,21 +18,67 @@ const TaskPage = () => {
       await deleteTask(taskKey);
       navigate(routes.todo(listNum));
     } catch (err) {
-      console.error(err);
+      setIsErrorShown(true);
+    }
+  };
+
+  const handleChange = async (taskKey) => {
+    try {
+      await updateTask(taskKey, { ...task, completed: !task.completed });
+    } catch (err) {
+      setIsErrorShown(true);
     }
   };
 
   return (
-    <>
-      <Link to={routes.todo(listNum)}>Back</Link>
-      <p>{taskKey} | {task.id}</p>
-      <Button as={Link} to={routes.editForm(listNum, taskId)} variant="link">
-        Edit
-      </Button>
-      <Button type="button" variant="link" onClick={handleDelete}>
-        Delete
-      </Button>
-    </>
+    <Main>
+      <NavPane reference={routes.todo(listNum)} title="Task Info" />
+
+      <div className="task-info">
+        <div className="todo-item">
+          <Checkbox
+            className="todo-checkbox rounded-4"
+            id={task.id}
+            checked={task.completed}
+            onChange={() => handleChange(taskKey)}
+          />
+          <span>{task.title}</span>
+        </div>
+        <p>{task.description || 'No description yet.'}</p>
+      </div>
+
+      <div className="menu d-flex align-items-center">
+        <Button
+          className="text-dark py-0 px-4 border-end"
+          type="button"
+          variant="link"
+          onClick={handleDelete}
+        >
+          <i className="bi bi-trash3"></i>
+        </Button>
+        <Button
+          className="text-dark py-0 px-4"
+          as={Link}
+          to={routes.editForm(listNum, taskId)}
+          variant="link"
+        >
+          <i className="bi bi-pencil"></i>
+        </Button>
+        <Button
+          className="ms-auto"
+          variant={task.completed ? 'primary' : 'success'}
+          onClick={() => handleChange(taskKey)}
+        >
+          {task.completed ? 'Undone' : 'Complete'}
+          {!task.completed && <i className="bi bi-check-lg ms-3"></i>}
+        </Button>
+      </div>
+
+      <ErrorMessage
+        show={isErrorShown}
+        handleClose={() => setIsErrorShown(false)}
+      />
+    </Main>
   );
 };
 
