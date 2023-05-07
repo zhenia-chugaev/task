bb584cd553c3cfee96e9b941909fbeab86c8ee8d
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-05-07 15:52:51 +0300

    Add infinite scrolling in calendar

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -7,49 +7,59 @@ import * as Time from '../utils/time';
 import { getTasksDataByDay } from '../utils/tasks';
 import { routes } from '../routes';
 
+const isScrollOver = (element) => {
+  const scrollWidth = Math.ceil(element.scrollLeft + element.clientWidth);
+  return scrollWidth >= element.scrollWidth;
+};
+
 const Calendar = () => {
   const { listNum } = useParams();
   const [selectedDay, setSelectedDay] = useState(Number(listNum));
+  const [daysCount, setDaysCount] = useState(30);
   const { tasks } = useDatabase();
 
-  const dayOfTheMonth = Time.getDay();
-  const daysLeft = (Time.daysInMonth() - dayOfTheMonth) + 1;
+  const handleScroll = (e) => {
+    if (isScrollOver(e.target)) {
+      setDaysCount(daysCount + 30);
+    }
+  };
 
   return (
-    <div className="calendar">
+    <div className="calendar" onScroll={handleScroll}>
       <Stack direction="horizontal" gap={3}>
-        {Array.from({ length: daysLeft }, (_, i) => dayOfTheMonth + i)
-          .map((day, i) => {
-            const dayTasks = getTasksDataByDay(tasks, i).map(([_, task]) => task);
-            const dayName = Time.getDayName(Date.now() + i * Time.MSEC_PER_DAY);
+        {Array.from({ length: daysCount }, (_, i) => {
+          const dayTasks = getTasksDataByDay(tasks, i).map(([_, task]) => task);
+          const dayTimestamp = Time.today() + i * Time.MSEC_PER_DAY;
+          const dayName = Time.getDayName(dayTimestamp);
+          const day = Time.getDay(dayTimestamp);
 
-            const hasCompletedTasks = dayTasks.some((task) => task.completed);
-            const hasIncompletedTasks = dayTasks.some((task) => !task.completed);
+          const hasCompletedTasks = dayTasks.some((task) => task.completed);
+          const hasIncompletedTasks = dayTasks.some((task) => !task.completed);
 
-            const dayClassName = cn('calendar-day', 'text-center', 'rounded-4', {
-              'bg-dark': i === selectedDay,
-              'text-light': i === selectedDay,
-              'text-primary': i !== selectedDay && dayName === 'Sun',
-              'border-primary': i !== selectedDay && dayName === 'Sun',
-            });
+          const dayClassName = cn('calendar-day', 'text-center', 'rounded-4', {
+            'bg-dark': i === selectedDay,
+            'text-light': i === selectedDay,
+            'text-primary': i !== selectedDay && dayName === 'Sun',
+            'border-primary': i !== selectedDay && dayName === 'Sun',
+          });
 
-            const barClassName = cn('status-bar', {
-              'has-completed': hasCompletedTasks,
-              'has-incompleted': hasIncompletedTasks,
-            });
+          const barClassName = cn('status-bar', {
+            'has-completed': hasCompletedTasks,
+            'has-incompleted': hasIncompletedTasks,
+          });
 
-            return (
-              <div key={i} onClick={() => setSelectedDay(i)}>
-                <Card className={dayClassName} as={Link} to={routes.todo(i)}>
-                  <Card.Body>
-                    <span className="d-block mb-1">{dayName}</span>
-                    <span className="fw-bold">{day}</span>
-                  </Card.Body>
-                </Card>
-                <div className={barClassName}></div>
-              </div>
-            );
-          })}
+          return (
+            <div key={i} onClick={() => setSelectedDay(i)}>
+              <Card className={dayClassName} as={Link} to={routes.todo(i)}>
+                <Card.Body>
+                  <span className="d-block mb-1">{dayName}</span>
+                  <span className="fw-bold">{day}</span>
+                </Card.Body>
+              </Card>
+              <div className={barClassName}></div>
+            </div>
+          );
+        })}
       </Stack>
     </div>
   );
