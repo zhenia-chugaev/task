80c063189c725866e4e1af0953486cf60bcf9876
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-08-30 16:19:53 +0300

    Initialize Firebase app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG);
+const firebaseApp = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
