832800a4f33ccd1bbe25325db3ca2d64bff94562
zhenia-chugaev <zhenia.chugaev@bk.ru>
2023-09-01 14:21:56 +0300

    Fix authentication context

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,7 +1,8 @@
 import { createContext } from 'react';
 
 const AuthContext = createContext({
-  isUserLoggedIn: false,
+  user: null,
+  signUp: () => {},
   logIn: () => {},
   logOut: () => {},
 });
